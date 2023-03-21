4c9103ab271fca2ddbc6f63cbc5a318ff400e080
barca0812 <jullion.stephane0812@gmail.com>
2023-03-21 15:14:43 +0100

    navbar footer index

---
+++ assets/includes/navbar.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="logo"><a class="navbar-brand" href="#"><img src="./assets/images/logo/logo.png" width="auto" height="70px"></a></div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Accueil
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Films</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nouveautés</a>
+          </li>
+
+          
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-sm-2" type="recherche" placeholder="Recherche">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Recherche</button>
+        </form>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Mon compte</a>
+          </li>
+      </div>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Mon compte
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+    
+  </nav>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</html>
